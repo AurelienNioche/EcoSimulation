8792404faa3ad1771e3212579023fc9bc4c3b920
AurelienNioche <nioche.aurelien@gmail.com>
2016-08-18 13:37:58 +0200

    Minor modifications for Avakas

--- upload-data.sh
+++ upload-data.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-echo "Compress files before sending..."
-ssh anioche@avakas.mcia.univ-bordeaux.fr
-cd aurelien/EcoSimulation-master
-tar -cf data.tar data
-logout
-echo "Files ready to be sent."
+# echo "Compress files before sending..."
+# ssh anioche@avakas.mcia.univ-bordeaux.fr
+# cd aurelien/EcoSimulation-master
+# tar -cf data.tar data
+# logout
+# echo "Files ready to be sent."
 scp -r anioche@avakas.mcia.univ-bordeaux.fr:/home/anioche/aurelien/EcoSimulation-master/data.tar /Users/M-E4-ANIOCHE/Desktop/EcoSimulation-data.tar
 echo "Decompress files"
 cd /Users/M-E4-ANIOCHE/Desktop
-tar -xf EcoSimulation-data.tar
+tar -xvf EcoSimulation-data.tar
 echo "Done!"
