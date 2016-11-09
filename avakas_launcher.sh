d459ce08e246c56bb9dffda11b323ba0dfb9b08f
AurelienNioche <nioche.aurelien@gmail.com>
2016-11-09 17:55:51 +0100

    Minor modifications for Avakas

--- avakas_launcher.sh
+++ avakas_launcher.sh
@@ -4,7 +4,7 @@ echo "Switch to python 2.7.12"
 pyenv local 2.7.12
 
 echo "Call 'avakas_launcher.py'"
-python avakas/avakas_launcher.py
+python module/avakas_launcher.py
 
 echo "Come back to python 3.5.2"
 pyenv local 3.5.2
